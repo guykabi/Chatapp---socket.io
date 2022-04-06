580e5371c53221b51d6e66dd5580b708f0c238cb
Guy Kabilio <99081637+guykabi@users.noreply.github.com>
2022-04-06 09:55:34 +0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { UsernameComponent } from './username/username.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    UsernameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ username.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.scss']
+})
+export class UsernameComponent {
+  @Output() userNameEvent = new EventEmitter<string>();
+
+  userName = '';
+  constructor() { }
+  
+  setUserName(): void {
+    this.userNameEvent.emit(this.userName);
+  }
+
+  
+
+}
